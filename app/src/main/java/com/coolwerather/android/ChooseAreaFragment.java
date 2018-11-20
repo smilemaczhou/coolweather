62870f391a8795b94811d71b0ba5a48af0377e64
zh <zh@zhdeMacBook-Pro.local>
2018-11-20 22:41:36 +0800

    get Data

--- app/src/main/java/com/coolwerather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolwerather/android/ChooseAreaFragment.java
@@ -1,6 +1,129 @@
 package com.coolwerather.android;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.coolwerather.android.db.City;
+import com.coolwerather.android.db.County;
+import com.coolwerather.android.db.Province;
+
+import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChooseAreaFragment extends Fragment {
+
+    public static final int LEVEL_PROVINCE = 0;
+
+    public static final int LEVE_CITY = 1;
+
+    public static final int LEVE_COUNTY = 2;
+
+    private ProgressDialog progressDialog;
+
+    private TextView titleText;
+
+    private Button backButton;
+
+    private ListView listView;
+
+    private ArrayAdapter<String> adapter;
+
+    private List<String> dataList = new ArrayList<>();
+
+    private List<Province> provinceList;
+
+    private List<City> cityList;
+
+    private List<County> countyList;
+
+    private Province selectedProvince;
+
+    private City selectedCity;
+
+    private County selectedCounty;
+
+    private int currenLevel;
+
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState) {
+        View view = inflater.inflate(R.layout.choose_area, container,false);
+        titleText = (TextView) view.findViewById(R.id.title_text);
+        backButton = (Button) view.findViewById(R.id.back_button);
+        listView = (ListView) view.findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated( Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (currenLevel == LEVEL_PROVINCE) {
+                    selectedProvince = provinceList.get(position);
+                    queryCities();
+                } else if (currenLevel == LEVE_CITY) {
+                    selectedCity = cityList.get(position);
+                    queryCounties();
+                }
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currenLevel == LEVE_COUNTY){
+                    queryCities();
+                } else if (currenLevel == LEVEL_PROVINCE) {
+                    queryProvince();
+                }
+            }
+        });
+        queryProvince();
+    }
+
+    private void queryProvince() {
+        titleText.setText("省份");
+        backButton.setVisibility(View.GONE);
+        provinceList = LitePal.findAll(Province.class);
+        if (provinceList.size()>0) {
+            dataList.clear();
+            for (Province province : provinceList) {
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currenLevel = LEVEL_PROVINCE;
+        } else {
+            String address = "http://guolin.tech/api/china";
+            queryFromServer(address, "province");
+        }
+    }
+
+    private void queryCities() {
+    }
+
+    private void queryCounties() {
+    }
+
+    private void queryFromServer(String address, final String type){
+
+    }
+
 }
--- app/src/main/java/com/coolwerather/android/util/Utility.java
+++ app/src/main/java/com/coolwerather/android/util/Utility.java
@@ -2,6 +2,8 @@ package com.coolwerather.android.util;
 
 import android.text.TextUtils;
 
+import com.coolwerather.android.db.City;
+import com.coolwerather.android.db.County;
 import com.coolwerather.android.db.Province;
 
 import org.json.JSONArray;
@@ -10,17 +12,58 @@ import org.json.JSONObject;
 
 public class Utility {
 
-    public static boolean handleProvinceResponse(String response){
-        if (!TextUtils.isEmpty(response)){
+    public static boolean handleProvinceResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
             try {
                 JSONArray allProvince = new JSONArray(response);
-                for (int i = 0; i < allProvince.length(); i++){
+                for (int i = 0; i < allProvince.length(); i++) {
                     JSONObject provinceObject = allProvince.getJSONObject(i);
                     Province province = new Province();
                     province.setProvinceName(provinceObject.getString("name"));
                     province.setProvinceCode(provinceObject.getInt("id"));
                     province.save();
                 }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCityResponse(String response, int provinceId) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0; i < allCities.length(); i++) {
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCountyResponse(String response, int cityId) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCounties = new JSONArray(response);
+                for (int i = 0; i < allCounties.length(); i++) {
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
             } catch (JSONException e) {
                 e.printStackTrace();
             }
