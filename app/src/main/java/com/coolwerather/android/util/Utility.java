c618e85a5d170b0f7efa965d5e2ce5f587d437de
zh <zh@zhdeMacBook-Pro.local>
2018-11-20 20:56:33 +0800

    json

--- app/src/main/java/com/coolwerather/android/util/Utility.java
+++ app/src/main/java/com/coolwerather/android/util/Utility.java
@@ -1,4 +1,30 @@
 package com.coolwerather.android.util;
 
+import android.text.TextUtils;
+
+import com.coolwerather.android.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Utility {
+
+    public static boolean handleProvinceResponse(String response){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allProvince = new JSONArray(response);
+                for (int i = 0; i < allProvince.length(); i++){
+                    JSONObject provinceObject = allProvince.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
 }
